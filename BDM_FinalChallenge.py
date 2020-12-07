fc8b845e40383b23a4338ce8e0b20359e7a6f6db
taqace <39810855+taqace@users.noreply.github.com>
2020-12-06 23:41:59 -0500

    Update BDM_FinalChallenge.py

--- BDM_FinalChallenge.py
+++ BDM_FinalChallenge.py
@@ -1,49 +1,19 @@
 import sys
 
+from pyspark import StorageLevel
 from pyspark.sql.dataframe import DataFrame
-from pyspark.sql.functions import udf
 from pyspark.sql.session import SparkSession
-from pyspark.sql.types import IntegerType
 from pyspark.sql import functions as F
-
+from pyspark.sql.types import IntegerType
 
 def get_spark_session() -> SparkSession:
     """getting spark session"""
     spark = SparkSession.builder.appName("parking_violations_statistics").getOrCreate()
+    #spark.conf.set("spark.sql.autoBroadcastJoinThreshold", -1)
+    #spark.conf.set("spark.sql.join.preferSortMergeJoin", False)
+    #spark.conf.set("spark.sql.inMemoryColumnarStorage.compressed", True)
+    #spark.conf.set("spark.sql.inMemoryColumnarStorage.batchSize", 50000)
     return spark
-
-
-@udf(returnType=IntegerType())
-def map_county_to_borough_number(county: str) -> str:
-    """
-    Function to convert county information to get borough code.
-    :param county:
-    :return:
-    """
-    _borough_name_number_map = {"MANHATTAN": 1, "BRONX": 2, "BROOKLYN": 3, "QUEENS": 4, "STATEN ISLAND": 5}
-    _county_borough_map = {
-        "MAN": _borough_name_number_map["MANHATTAN"],
-        "MH": _borough_name_number_map["MANHATTAN"],
-        "MN": _borough_name_number_map["MANHATTAN"],
-        "NEWY": _borough_name_number_map["MANHATTAN"],
-        "NEW Y": _borough_name_number_map["MANHATTAN"],
-        "NY": _borough_name_number_map["MANHATTAN"],
-        "BRONX": _borough_name_number_map["BRONX"],
-        "BX": _borough_name_number_map["BRONX"],
-        "BK": _borough_name_number_map["BROOKLYN"],
-        "K": _borough_name_number_map["BROOKLYN"],
-        "KING": _borough_name_number_map["BROOKLYN"],
-        "KINGS": _borough_name_number_map["BROOKLYN"],
-        "Q": _borough_name_number_map["QUEENS"],
-        "QN": _borough_name_number_map["QUEENS"],
-        "QNS": _borough_name_number_map["QUEENS"],
-        "QU": _borough_name_number_map["QUEENS"],
-        "QUEEN": _borough_name_number_map["QUEENS"],
-        "R": _borough_name_number_map["STATEN ISLAND"],
-        "RICHMOND": _borough_name_number_map["STATEN ISLAND"],
-    }
-
-    return _county_borough_map.get(county, -99)
 
 
 def read_csv_file(spark: SparkSession, path: str) -> DataFrame:
@@ -65,18 +35,34 @@ def transform_parking_violation_data(df: DataFrame, column: str = "Violation Cou
     4. Removed any data having no house number
     """
 
-    df = df.withColumn("BOROCODE", map_county_to_borough_number(column))
     df = (
-        df.withColumn("temp", F.split("House Number", "-"))
-        .withColumn("L_HOUSE_NO", F.col("temp").getItem(0).cast("int"))
-        .withColumn(
-            "H_HOUSE_NO",
-            F.when(F.col("temp").getItem(1).isNull(), "9999").otherwise(F.col("temp").getItem(1)).cast("int"),
-        )
-        .drop("temp")
-        .filter(F.col("L_HOUSE_NO").isNotNull())
-        .withColumn("Street Name", F.upper(F.col("Street Name")))
+        df.select("Violation County", "House Number", "Street Name", "Summons Number", "Issue Date")
         .distinct()
+        .withColumn("year", F.year(F.to_date(F.col("Issue Date"), "MM/dd/yyyy"))).orderBy("Violation County", "House Number", "Street Name", "year")
+        .coalesce(100)
+        .groupBy("Violation County", "House Number", "Street Name", "year")
+        .agg({"Summons Number": "count"})
+        .withColumnRenamed("count(Summons Number)", "total_cnt")
+        .withColumn(
+            "BOROCODE",
+            F.when(F.col(column).isin(["MAN", "MH", "MN", "NEWY", "NEW Y", "NY"]), 1)
+            .when(F.col(column).isin(["BRONX", "BX"]), 2)
+            .when(F.col(column).isin(["BK", "K", "KING", "KINGS"]), 3)
+            .when(F.col(column).isin(["Q", "QN", "QNS", "QU", "QUEEN"]), 4)
+            .when(F.col(column).isin(["R", "RICHMOND"]), 5)
+            .otherwise(0),
+        )
+    )
+
+    df = (
+        df.filter(F.col("House Number").isNotNull())
+        .withColumn("temp", F.split("House Number", "-"))
+        .withColumn(
+            "House Number",
+            F.col("temp").getItem(0).cast("int")
+            + F.when(F.col("temp").getItem(1).isNull(), "0").otherwise(F.col("temp").getItem(1)).cast("int") / 1000,
+        ).withColumn("temp",F.col("temp").getItem(0).cast("int"))
+        .withColumn("Street Name", F.upper(F.col("Street Name")))
     )
     return df
 
@@ -95,54 +81,37 @@ def transform_read_centerline_data(df: DataFrame) -> DataFrame:
     4. Removed any data having no house number in L_LOW_HN and R_LOW_HN
     """
     df = (
-        df.withColumn("ST_LABEL", F.upper(F.col("ST_LABEL")))
+        df.select("PHYSICALID", "BOROCODE", "FULL_STREE", "ST_NAME", "L_LOW_HN", "L_HIGH_HN", "R_LOW_HN", "R_HIGH_HN").orderBy("PHYSICALID", "BOROCODE", "FULL_STREE", "ST_NAME", "L_LOW_HN", "L_HIGH_HN", "R_LOW_HN", "R_HIGH_HN").coalesce(200)
+        .withColumn("ST_NAME", F.upper(F.col("ST_NAME")))
         .withColumn("FULL_STREE", F.upper(F.col("FULL_STREE")))
         .filter((F.col("L_LOW_HN").isNotNull()) | (F.col("R_LOW_HN").isNotNull()))
     )
-    df = (
-        df.withColumn("L_TEMP_ODD", F.split("L_LOW_HN", "-"))
-        .withColumn("LOWER_LOW_HOUSE_NO_ODD", F.col("L_TEMP_ODD").getItem(0).cast("int"))
-        .withColumn(
-            "UPPER_LOW_HOUSE_NO_ODD",
-            F.when(F.col("L_TEMP_ODD").getItem(1).isNull(), "9000")
-            .otherwise(F.col("L_TEMP_ODD").getItem(1))
-            .cast("int"),
-        )
-        .drop("L_TEMP_ODD")
-    )
-    df = (
-        df.withColumn("L_TEMP_ODD", F.split("L_HIGH_HN", "-"))
-        .withColumn("LOWER_HIGH_HOUSE_NO_ODD", F.col("L_TEMP_ODD").getItem(0).cast("int"))
-        .withColumn(
-            "UPPER_HIGH_HOUSE_NO_ODD",
-            F.when(F.col("L_TEMP_ODD").getItem(1).isNull(), "9999")
-            .otherwise(F.col("L_TEMP_ODD").getItem(1))
-            .cast("int"),
-        )
-        .drop("L_TEMP_ODD")
+    df = df.withColumn("L_TEMP_ODD", F.split("L_LOW_HN", "-")).withColumn(
+        "L_LOW_HN",
+        F.col("L_TEMP_ODD").getItem(0).cast("int")
+        + F.when(F.col("L_TEMP_ODD").getItem(1).isNull(), "0").otherwise(F.col("L_TEMP_ODD").getItem(1)).cast("int")
+        / 1000,
     )
 
-    df = (
-        df.withColumn("R_TEMP_EVEN", F.split("R_LOW_HN", "-"))
-        .withColumn("LOWER_LOW_HOUSE_NO_EVEN", F.col("R_TEMP_EVEN").getItem(0).cast("int"))
-        .withColumn(
-            "UPPER_LOW_HOUSE_NO_EVEN",
-            F.when(F.col("R_TEMP_EVEN").getItem(1).isNull(), "9000")
-            .otherwise(F.col("R_TEMP_EVEN").getItem(1))
-            .cast("int"),
-        )
-        .drop("R_TEMP_EVEN")
+    df = df.withColumn("L_TEMP_ODD", F.split("L_HIGH_HN", "-")).withColumn(
+        "L_HIGH_HN",
+        F.col("L_TEMP_ODD").getItem(0).cast("int")
+        + F.when(F.col("L_TEMP_ODD").getItem(1).isNull(), "0").otherwise(F.col("L_TEMP_ODD").getItem(1)).cast("int")
+        / 1000,
     )
-    df = (
-        df.withColumn("R_TEMP_EVEN", F.split("R_HIGH_HN", "-"))
-        .withColumn("LOWER_HIGH_HOUSE_NO_EVEN", F.col("R_TEMP_EVEN").getItem(0).cast("int"))
-        .withColumn(
-            "UPPER_HIGH_HOUSE_NO_EVEN",
-            F.when(F.col("R_TEMP_EVEN").getItem(1).isNull(), "9999")
-            .otherwise(F.col("R_TEMP_EVEN").getItem(1))
-            .cast("int"),
-        )
-        .drop("R_TEMP_EVEN")
+
+    df = df.withColumn("L_TEMP_ODD", F.split("R_LOW_HN", "-")).withColumn(
+        "R_LOW_HN",
+        F.col("L_TEMP_ODD").getItem(0).cast("int")
+        + F.when(F.col("L_TEMP_ODD").getItem(1).isNull(), "0").otherwise(F.col("L_TEMP_ODD").getItem(1)).cast("int")
+        / 1000,
+    )
+
+    df = df.withColumn("L_TEMP_ODD", F.split("R_HIGH_HN", "-")).withColumn(
+        "R_HIGH_HN",
+        F.col("L_TEMP_ODD").getItem(0).cast("int")
+        + F.when(F.col("L_TEMP_ODD").getItem(1).isNull(), "0").otherwise(F.col("L_TEMP_ODD").getItem(1)).cast("int")
+        / 1000,
     )
 
     return df
@@ -161,62 +130,63 @@ def join_park_violation_with_centerline(df_park_violation: DataFrame, df_centerl
     :param df_centerline:
     :return:
     """
-    df_park_violation.cache()
+    # df_park_violation = df_park_violation.repartition("BOROCODE", "Street Name", "House Number")
+    # df_centerline.cache()
 
     """below steps for even house number"""
-    df_joined_even = (
-        df_park_violation.filter(F.col("House Number") % 2 == 0)
-        .alias("park")
-        .join(
-            F.broadcast(df_centerline).alias("centerline"),
-            ((F.col("Street Name") == F.col("ST_NAME")) | (F.col("Street Name") == F.col("FULL_STREE")))
-            & (F.col("park.BOROCODE") == F.col("centerline.BOROCODE"))
-            & (
-                F.col("park.L_HOUSE_NO").between(
-                    F.col("centerline.LOWER_LOW_HOUSE_NO_EVEN"), F.col("centerline.LOWER_HIGH_HOUSE_NO_EVEN")
-                )
-            )
-            & (
-                F.col("park.H_HOUSE_NO").between(
-                    F.col("centerline.UPPER_LOW_HOUSE_NO_EVEN"), F.col("centerline.UPPER_HIGH_HOUSE_NO_EVEN")
-                )
-            ),
-        )
-        .select("Summons Number", "Issue Date", "PHYSICALID")
-    )
 
     """below steps for odd house number"""
+    df_park_violation.cache()
+    df_centerline.cache()
 
-    df_joined_odd = (
-        df_park_violation.filter(F.col("House Number") % 2 != 0)
+    df_park_violation_odd = df_park_violation.filter(F.col("temp") % 2 != 0)
+    df_park_violation_even = df_park_violation.filter(F.col("temp") % 2 == 0)
+    df_centerline.count()
+
+    df_joined_1 = (
+        df_park_violation_even
         .alias("park")
         .join(
-            F.broadcast(df_centerline).alias("centerline"),
+            df_centerline.alias("centerline").hint("broadcast"),
             ((F.col("Street Name") == F.col("ST_NAME")) | (F.col("Street Name") == F.col("FULL_STREE")))
             & (F.col("park.BOROCODE") == F.col("centerline.BOROCODE"))
             & (
-                F.col("park.L_HOUSE_NO").between(
-                    F.col("centerline.LOWER_LOW_HOUSE_NO_ODD"), F.col("centerline.LOWER_HIGH_HOUSE_NO_ODD")
-                )
-            )
-            & (
-                F.col("park.H_HOUSE_NO").between(
-                    F.col("centerline.UPPER_LOW_HOUSE_NO_ODD"), F.col("centerline.UPPER_HIGH_HOUSE_NO_ODD")
-                )
+                (F.col("park.House Number") >= F.col("centerline.R_LOW_HN"))
+                & (F.col("park.House Number") <= F.col("centerline.R_HIGH_HN"))
             ),
         )
-        .select("Summons Number", "Issue Date", "PHYSICALID")
+        .select("total_cnt", "year", "PHYSICALID")
+    )
+
+    df_joined_2 = (
+        df_park_violation_odd
+        .alias("park")
+        .join(
+            df_centerline.alias("centerline").hint("broadcast"),
+            ((F.col("Street Name") == F.col("ST_NAME")) | (F.col("Street Name") == F.col("FULL_STREE")))
+            & (F.col("park.BOROCODE") == F.col("centerline.BOROCODE"))
+            & (
+                (F.col("park.House Number") >= F.col("centerline.L_LOW_HN"))
+                & (F.col("park.House Number") <= F.col("centerline.L_LOW_HN"))
+            ),
+        )
+        .select("total_cnt", "year", "PHYSICALID")
     )
 
     """returing union of 2 dataframes"""
-    return df_joined_even.union(df_joined_odd)
+    return df_joined_1.unionAll(df_joined_2)
 
 
 def aggregate_dataset_by_year(joined_df: DataFrame) -> DataFrame:
     """aggregating the data based on 'PHYSICALID' and 'Year' of issue date and sorting based on 'PHYSICALID'"""
 
-    df = joined_df.withColumn("year", F.year(F.to_date(F.col("Issue Date"), "MM/dd/yyyy")))
-    df = df.groupBy("PHYSICALID").pivot("year", [2015, 2016, 2017, 2018, 2019]).count()
+    # df = joined_df.withColumn("year", F.year(F.to_date(F.col("Issue Date"), "MM/dd/yyyy")))
+    df = (
+        joined_df.repartition(5, "year")
+        .groupBy("PHYSICALID")
+        .pivot("year", [2015, 2016, 2017, 2018, 2019])
+        .sum("total_cnt")
+    )
     df = (
         df.withColumn("2015", F.when(F.col("2015").isNull(), 0).otherwise(F.col("2015")))
         .withColumnRenamed("2015", "COUNT_2015")
@@ -242,15 +212,16 @@ if __name__ == "__main__":
     """Main entrypoint"""
     spark = get_spark_session()
     centerline_date_path = "hdfs:///data/share/bdm/nyc_cscl.csv"  # TODO: Provide correct hdfs path for centerline
-    parking_violation_data_path = "hdfs:///data/share/bdm/nyc_parking_violation"  # TODO: Provide correct hdfs path for parking violation
+    parking_violation_data_path = "hdfs:///data/share/bdm/nyc_parking_violation/"  # TODO: Provide correct hdfs path for parking violation
     """read output path from commandline"""
     output_path = sys.argv[1]
     df_park_violation = read_parking_violation_data(spark, parking_violation_data_path)
     df_park_violation = transform_parking_violation_data(df_park_violation)
+    # df_park_violation.show()
     centerline = read_centerline_data(spark, centerline_date_path)
     centerline = transform_read_centerline_data(centerline)
+    # df_park_violation.show()
     df = join_park_violation_with_centerline(df_park_violation, centerline)
     df = aggregate_dataset_by_year(df)
     write_output(df, output_path)
-    #hdfs:///user/pjohnso002/output for outputfolder
-    
+    # df.filter(F.col("PHYSICALID").isin([166776, 89620, 79863, 48068])).show()  # TODO: remove later
